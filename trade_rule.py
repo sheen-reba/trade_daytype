62f2d173a723898cd51ffedfbed215f4a4d09c5d
sheen-reba <3466170276@qq.com>
2024-11-27 12:35:06 +0800

    获取代码基本完成

--- trade_rule.py
+++ trade_rule.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-dire='./trade_daytype'
+dire='./trade_rule'
 mode=(1,0)
 gtype=1
 
@@ -44,23 +44,22 @@ def __change_dire(old,new):
     import shutil
     shutil.move(old,new)
 
-def __decrypt(name,loads,decode,lz=None):
+def __load(name,load,lz=None):
     f=open(name,'rb')
     if lz is not None:
         lzd=lz.decompress
-        data=loads(decode(lzd(f.read())))
+        data=load(lzd(f.read()))
     else:
-        data=loads(decode(f.read()))
+        data=load(f)
     f.close()
     return data
 
-def __result(name,load):
-    f=open(name,'rb')
-    data=load(f)
+def __dump(name,data,dump):
+    f=open(name,'wb')
+    dump(data,f)
     f.close()
-    return data
 
-def main(mod:tuple[int,int],gtyp:int=None,ori='trade_rule',
+def main(mod:tuple[int,int],gtyp:int=None,ori='trade_rule/',
          pre0={
              (1,0):'stock_all',(1,1):'stock_rule',(1,2):'stock_info',(1,3):'stock_dates',
              (2,0):'future_all',(2,1):'future_rule',(2,2):'future_info',(2,3):'future_dates',
@@ -110,25 +109,86 @@ def main(mod:tuple[int,int],gtyp:int=None,ori='trade_rule',
             尝试去解压缩文件到工作路径，成功则返回数据，返回前判断本文件中提示参数（三个）是否需要更改（按优先级去更改、移动文件夹与文件）
         还未成功就从指定网址去按获取方式下载（如果需要解压缩就会解压）到工作路径中去，之后返回数据，返回前判断本文件中提示参数是否需要更改（按优先级去更改、移动文件夹与文件）
     '''
-    import os,pickle
+    import os,pickle,datetime #datetime给数据中的时间数据提供实例化支持
     exists=os.path.exists
-    work=os.getcwd().replace('\\','/') #'D:\\projects\\trade_daytype'
-    path=os.path.abspath(__file__).replace('\\','/') #'D:\\projects\\trade_daytype\\trade_rule.py'
-    objpath=work+'/'+ori
-    if exists(objpath):
-        if exists(objpath+'/'+pre0[mod]):
-            return __result(objpath+'/'+pre0[mod],pickle.load)
-        elif gtyp==1 and exists(objpath+'/'+pre1[mod]):
-            __decrypt()
-        __change_self(dir,mod,gtyp,file,sig=0)
-    __change_self(work,2,2,path)
-    if mod!=mode:
-        ...
-    import datetime,pickle,base64,lzma,requests
-    a=requests.get('https://api.github.com/repos/sheen-reba/trade_daytype/contents/api.py?ref=main')
-    b=a.json()
-    # c=b64decode(b['content']).decode('utf-8')
-    # exec(c)
+    workdir=os.getcwd().replace('\\','/')+'/' #'D:\\projects\\trade_daytype'
+    self=os.path.abspath(__file__).replace('\\','/') #'D:\\projects\\trade_daytype\\trade_rule.py'
+    selfdir=self[:self.rfind('/')+1]
+    objdir=workdir+ori
+    if exists(objdir): #先检查工作路径下是否有该数据文件夹
+        if exists(objdir+pre0[mod]): #数据文件夹中是否有数据文件
+            __change_self(objdir,mod,gtyp,self)
+            return __load(objdir+pre0[mod],pickle.load)
+        elif gtyp==1 and exists(objdir+pre1[mod]):
+            import lzma
+            data=__load(objdir+pre1[mod],pickle.loads,lzma.LZMADecompressor(format=lzma.FORMAT_XZ).decompress)
+            __dump(objdir+pre0[mod],data,pickle.dump)
+            __change_self(objdir,mod,gtyp,self)
+            return data
+    if exists(workdir+pre0[mod]): #工作路径下是否有数据文件呢
+        os.makedirs(objdir,exist_ok=True)
+        __change_dire(workdir+pre0[mod],objdir)
+        __change_self(objdir,mod,gtyp,self)
+        return __load(objdir+pre0[mod],pickle.load)
+    elif gtyp==1 and exists(workdir+pre1[mod]):
+        import lzma
+        data=__load(workdir+pre1[mod],pickle.loads,lzma.LZMADecompressor(format=lzma.FORMAT_XZ).decompress)
+        os.makedirs(objdir,exist_ok=True)
+        __dump(objdir+pre0[mod],data,pickle.dump)
+        __change_self(objdir,mod,gtyp,self)
+        return data
+    #等于的时候不用再去判断了
+    if dire!=objdir and exists(dire): #默认路径下是否有数据文件夹呢（如果默认路径与工作路径不一致的话）
+        if exists(dire+pre0[mod]):
+            __change_dire(dire,workdir) #是要放到工作路径中，从而把数据的路径变成objdir
+            __change_self(objdir,mod,gtyp,self)
+            return __load(objdir+pre0[mod],pickle.load)
+        elif gtyp==1 and exists(dire+pre1[mod]):
+            import lzma
+            data=__load(dire+pre1[mod],pickle.loads,lzma.LZMADecompressor(format=lzma.FORMAT_XZ).decompress)
+            os.makedirs(objdir,exist_ok=True)
+            __dump(objdir+pre0[mod],data,pickle.dump)
+            __change_self(objdir,mod,gtyp,self)
+            return data
+    if selfdir!=objdir and selfdir!=dire and exists(selfdir):
+        if exists(selfdir+pre0[mod]):
+            __change_dire(selfdir,workdir) #是要放到工作路径中，从而把数据的路径变成objdir
+            __change_self(objdir,mod,gtyp,self)
+            return __load(objdir+pre0[mod],pickle.load)
+        elif gtyp==1 and exists(selfdir+pre1[mod]):
+            import lzma
+            data=__load(selfdir+pre1[mod],pickle.loads,lzma.LZMADecompressor(format=lzma.FORMAT_XZ).decompress)
+            os.makedirs(objdir,exist_ok=True)
+            __dump(objdir+pre0[mod],data,pickle.dump)
+            __change_self(objdir,mod,gtyp,self)
+            return data
+    print(f'{datetime.datetime.now()} 模块-trade_rule-正从指定连接获取给定数据')
+    os.makedirs(objdir,exist_ok=True)
+    if not gtyp:
+        import requests,base64
+        retu=requests.get(f'https://api.github.com/repos/sheen-reba/trade_daytype/contents/{pre0[mod]}?ref=main')
+        if retu.status_code==200:
+            data=base64.b64decode(retu.json()['content'])
+            f=open(objdir+pre0[mod],'wb')
+            f.write(data)
+            f.close()
+            __change_self(objdir,mod,gtyp,self)
+            return pickle.loads(data)
+    elif gtyp==1:
+        print(f'{datetime.datetime.now()} 模块-trade_rule-正从指定连接获取给定数据')
+        import requests,lzma,base64
+        retu=requests.get(f'https://api.github.com/repos/sheen-reba/trade_daytype/contents/{pre1[mod]}?ref=main')
+        if retu.status_code==200:
+            xz=base64.b64decode(retu.json()['content'])
+            f=open(objdir+pre1[mod],'wb')
+            f.write(xz)
+            f.close()
+            data=pickle.loads(lzma.LZMADecompressor(format=lzma.FORMAT_XZ).decompress(xz))
+            os.makedirs(objdir,exist_ok=True)
+            __dump(objdir+pre0[mod],data,pickle.dump)
+            __change_self(objdir,mod,gtyp,self)
+            return data
+    print(f'{datetime.datetime.now()} 模块-trade_rule-从指定连接获取给定数据失败，连接状态码为{retu.status_code}')
 
 if __name__=='__main__':
-    main()
+    main((3,0),1)
